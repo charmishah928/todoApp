e74c4b793cf2d55d527c75ea32adcd97dc93caf8
Charmi Shah <charmishah323@gmail.com>
2017-05-07 03:45:29 +0000

    rcav practice

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'list/index'
 
   root 'application#hello'
+  get 'list/1' => 'list#first'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
