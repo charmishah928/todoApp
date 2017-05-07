f26fb58ab0300a400fb26f0c5b60b914eb1213f2
Charmi Shah <charmishah323@gmail.com>
2017-05-07 13:41:36 +0000

    Dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     
     root 'application#hello'
+    get 'list/:id' => 'list#show'
     get 'list/1' => 'list#first'
     get 'list/2' => 'list#second'
     get 'list/index'
