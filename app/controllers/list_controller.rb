0aecce5e893c25aa17a57dc19362aca7c7a1e5d7
Charmi Shah <charmishah323@gmail.com>
2017-05-07 15:11:12 +0000

    changes

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,16 +1,20 @@
 class ListController < ApplicationController
   def index
+    #@assignment_one_description = "Complete all missed assignments - Physics"
+    #@assignment_one_pomodoro_estimate = "4 pomodoros"
+  end
+  
+  def first
     @assignment_one_description = "Complete all missed assignments - Physics"
     @assignment_one_pomodoro_estimate = "4 pomodoros"
   end
   
-  def first
-  end
-  
   def second
   end
-
-
+  
+  def show
+    @id = params ['id']
+  end
   
   
 end
